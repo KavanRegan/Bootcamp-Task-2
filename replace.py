fffdaf66c0ea8c8302126d0fe7e047111c577ab9
Kavan <kavan.regan@gmail.com>
2024-03-05 23:54:06 +0000

    Comments added

--- replace.py
+++ replace.py
@@ -1,8 +1,10 @@
+# - Create variable with ! to remove
 word = "The!quick!brown!fox!jumps!over!the!lazy!dog!"
+# - Using .replace swap the ! for a space
 word = word.replace("!", " ")
 print(word)
-
+# - Change the word to upper case
 word = word.upper()
 print(word)
-
+# - Print the word
 print(word[::-1])
